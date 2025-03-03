aa75beef3bb2154ff59e1045f0c2fc9bb1b1bfc2
jayshree <jayshree.a25@gmail.com>
2025-03-03 11:50:04 +0530

    Admin Subcategory changes

---
+++ application/models/Sub_category_model.php
@@ -0,0 +1,54 @@
+<?php
+
+class Sub_category_Model extends CI_Model
+{
+    public function save_sub_category_info($data)
+    {
+        return $this->db->insert('tbl_sub_category', $data);
+    }
+
+    public function getall_sub_category_info()
+    {
+
+        $this->db->select('*');
+        $this->db->from('tbl_sub_category');
+        $info = $this->db->get();
+        return $info->result();
+    }
+
+    public function edit_sub_category_info($id)
+    {
+        $this->db->select('*');
+        $this->db->from('tbl_sub_category');
+        $this->db->where('id', $id);
+        $info = $this->db->get();
+        return $info->row();
+    }
+
+    public function delete_sub_category_info($id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->delete('tbl_sub_category');
+    }
+
+    public function update_sub_category_info($data, $id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->update('tbl_sub_category', $data);
+    }
+
+    public function published_sub_category_info($id)
+    {
+        $this->db->set('publication_status', 1);
+        $this->db->where('id', $id);
+        return $this->db->update('tbl_sub_category');
+    }
+
+    public function unpublished_sub_category_info($id)
+    {
+        $this->db->set('publication_status', 0);
+        $this->db->where('id', $id);
+        return $this->db->update('tbl_sub_category');
+    }
+
+}
